daf065f7bf0b4a6d71c9af39b634ab4dd5c192c9
Lucas-Ying <lucas.yingmm@gmail.com>
2016-08-20 22:34:48 +1200

    modify scripts for linux

--- Q3/run.sh
+++ Q3/run.sh
@@ -2,7 +2,7 @@
 
 mkdir result
 
-./do.sh ../project1-images/3.1/mfeat-digits/
-./do_more.sh ../project1-images/3.1/mfeat-digits/
-./clean.sh
+bash ./do.sh ../project1-images/3.1/mfeat-digits/
+bash ./do_more.sh ../project1-images/3.1/mfeat-digits/
+bash ./clean.sh
 
